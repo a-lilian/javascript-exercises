8c8dc61b0beebc1b6437ef1ccc448b9e7ea7af8a
a_lilian <161257210+a-lilian@users.noreply.github.com>
2025-05-16 03:20:59 +0100

    Solve exercise 06 repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, repeatCount) {
 
+    if (repeatCount < 0) {
+        return "ERROR";
+    }
+    
+    let output = "";
+    
+    for (let i = 0; i < repeatCount; i ++)
+    {
+        output += string;
+    }
+    return output;
 };
 
 // Do not edit below this line
