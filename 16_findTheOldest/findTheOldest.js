516618ba0e35dd08481f00de2a3cf16d4bdb29bd
a_lilian <161257210+a-lilian@users.noreply.github.com>
2025-05-21 23:32:44 +0100

    Solve exercise 16 findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+function getAge (person) {
+    if (person.yearOfDeath === undefined) {
+        person.yearOfDeath = new Date().getFullYear();
+        //gosh I hope I got the scope right for this block 🐴
+    }
 
+    return person.yearOfDeath - person.yearOfBirth;
+}
+
+const findTheOldest = function(arr) {
+    arr.sort((person1, person2) => getAge(person2) - getAge(person1));
+    return arr[0];
 };
 
 // Do not edit below this line
