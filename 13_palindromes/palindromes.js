84348f0205adc9c31840827c74e3645fd424756f
a_lilian <161257210+a-lilian@users.noreply.github.com>
2025-05-21 22:54:53 +0100

    Solve exercise 13 palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+function stripStrings (inputString) {
+    const toRemove = /[\p{P}\s]/gu
+    return inputString.replaceAll(toRemove, "").toLowerCase();
+} 
 
+const palindromes = function (input) {
+    input = stripStrings(input);
+    const inputReversed = input.split("").reverse().join("");
+    return input === inputReversed;
 };
 
 // Do not edit below this line
