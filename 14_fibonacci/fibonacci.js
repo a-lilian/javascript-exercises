f37dbdc4278a6deab5b3968fa5690d15a7b1231c
a_lilian <161257210+a-lilian@users.noreply.github.com>
2025-05-21 23:14:10 +0100

    Solve exercise 14 fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = Number(n);
 
+    if (n < 0) {
+        return "OOPS";
+    }
+
+    let fibonacciNumbers = [0, 1];
+
+    for (let i = 2; i <= n; i ++) {
+        fibonacciNumbers[i] = 
+        fibonacciNumbers[i - 1] + 
+        fibonacciNumbers[i - 2]; 
+    }
+
+    return fibonacciNumbers[n];
 };
 
 // Do not edit below this line
